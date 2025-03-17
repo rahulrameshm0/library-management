8a4c23cec4522ce660940f310de3f8aba29e8433
rahulrameshm0 <iamrahulm23@gmail.com>
2025-03-17 21:48:47 +0530

    created a update for book and user in admin.py

--- library/library_app/urls.py
+++ library/library_app/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('library/admin_login/admin_signup/', views.admin_signup, name='admin_signup'),
     path('library/admin_login/', views.admin_login, name='admin_login'),
     path('admin-dashboard/add-book', views.add_book, name='add_book'),
+    path('admin-dashboard/add-user', views.add_user, name='add_user'),
+    path('admin-dashboard/delete-user/<int:user_id>', views.delete_user, name='delete_user'),
+    path('admin-dashboard/update-user/<int:user_id>', views.update_user, name='update_user'),
+    path('admin-dashboard/delete-book/<int:book_id>', views.update_user, name='delete_book'),
+    path('admin-dashboard/update-book/<int:book_id>', views.update_book, name='update_book'),
 ]
--- library/library_app/views.py
+++ library/library_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.models import User
 from . models import Book,Admin
@@ -86,9 +86,9 @@ def book_list(request):
 def admin_dashboard(request):
     admin_user = Admin.objects.all()
     books = Book.objects.all()
+    users = User.objects.all() 
 
-    return render(request, 'admin.html', {'admin_user': admin_user, 'books': books})
-
+    return render(request, 'admin.html', {'admin_user': admin_user, 'books': books, 'users': users})
 def add_book(request):
     if request.method == "POST":
         title = request.POST.get("title")
@@ -102,9 +102,86 @@ def add_book(request):
             messages.error(request, "Please fill all fields")
         
         return redirect("admin_dashboard")
+    
+def user_list(request):
+    users  =User.objects.all()
+    return render(request, "user_list.html", {"users": users})
 
-def delete_book(request):
-    pass
+def add_user(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        if not username or not email or not password:
+            messages.error(request, "Please fill all the requierd field")
+            return redirect("admin_dashboard")
+        
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "This email address already exist")
+            return redirect("admin_dashboard")
+
+        new_user = User.objects.create_user(username=username,email=email, password=password)
+        new_user.save()
+        messages.success(request, "user has been successfully created!")
+        return redirect("admin_dashboard")
+    
+    return redirect("admin_dashboard")
+
+def delete_book(request, book_id):
+    book_obj = get_object_or_404(Book, id=book_id)
+    book_obj.delete()
+    messages.success(request, "Book has been successfully deleted!")
+    return redirect("admin_dashboard")
+
+def delete_user(request, user_id):
+    user_obj = get_object_or_404(User, id=user_id)
+    user_obj.delete()
+    messages.success(request, "Book has been successfully deleted!")
+    return redirect("admin_dashboard")
+
+def update_user(request, user_id):
+    user_obj = get_object_or_404(User, id=user_id)
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+
+        if not username or not email:
+            messages.error(request, "All fields are requered!")
+            return redirect("admin_dashboard")
+        
+        if User.objects.filter(email=email).exclude(id=user_id).exists():
+            messages.error(request, "This email already exists please try with the different email")
+            return redirect('admin_dashboard')
+        
+        user_obj.username = username
+        user_obj.email = email
+        user_obj.save()
+        messages.success(request, "username and email has been successfully updated")
+        return redirect("admin_dashboard") 
+
+    return render(request, "update_user.html", {"user": user_obj})
+
+def update_book(request, book_id):
+    book_obj = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        title = request.POST.get("title")
+        author = request.POST.get("author")
+        available_copy = request.POST.get("available_copy")
+
+        if not title or not author or not available_copy:
+            messages.error(request, "All fields are required!")
+            return redirect('admin_dashboard')
+        
+        book_obj.title = title
+        book_obj.author = author
+        book_obj.available_copy = available_copy
+        book_obj.save()
+        messages.success(request, "Book has been sucessfully updated")
+        return redirect('admin_dashboard')
+    
+    return render(request, "admin.html")
+
 
 def sign_out(request):
     pass
